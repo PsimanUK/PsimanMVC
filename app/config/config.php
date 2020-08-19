7ce93665b93732f8d28c5b8fd7211fbe16831b7e
PsimanUK <simeonparris@hotmail.com>
2020-08-19 13:14:57 +0100

    add header and footer files to views/includes so they can be required into each view page.

--- app/config/config.php
+++ app/config/config.php
@@ -1,6 +1,6 @@
 <?php
     // App Root
-    define('APPROOT', dirname(dirname(__FILE__)) . '<br>'); 
+    define('APPROOT', dirname(dirname(__FILE__))); 
 
     //URL Root
     define('URLROOT', 'http://localhost/PsimanMVC');
