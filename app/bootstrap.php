c40bb73c0f517f4a7bbc157d21f1448266eea2f4
PsimanUK <simeonparris@hotmail.com>
2020-08-19 13:02:16 +0100

    add functionality to autoload any php files in the libraries folder.

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,5 +1,8 @@
 <?php 
- // load in the libraries
- require_once 'libraries/Controller.php';
- require_once 'libraries/Core.php';
- require_once 'libraries/Database.php';
+    // Load config file
+    require_once 'config/config.php';
+
+    // Autoload Core Libraries
+    spl_autoload_register(function($className){
+        require_once 'libraries/' . $className . '.php';
+    });
