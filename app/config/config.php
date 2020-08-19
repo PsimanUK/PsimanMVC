15c6abb0c54d943f91f72719ba807bafb5dd84d3
PsimanUK <simeonparris@hotmail.com>
2020-08-19 15:28:37 +0100

    create constant variables in config.php for use when connecting to the database.

--- app/config/config.php
+++ app/config/config.php
@@ -1,4 +1,10 @@
 <?php
+    // Database parameters
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '123456');
+    define('DB_NAME', 'PsimanMVC_DB');
+
     // App Root
     define('APPROOT', dirname(dirname(__FILE__))); 
 
