a35f49681f25b42e4d7e5a906b9374d982dc61de
PsimanUK <simeonparris@hotmail.com>
2020-08-19 10:39:14 +0100

    add funtionality to core.php to change the current controller if the beginning of the url matches with an existing controller.

--- app/libraries/core.php
+++ app/libraries/core.php
@@ -12,7 +12,20 @@
                 // print_r($this->getUrl());
 
                 $currentUrl = $this->getUrl();
+                echo '<br> The First Section of the Current URL is ' . $currentUrl[0] . '<br>';
 
+                if(file_exists('../app/controllers/' . ucwords($currentUrl[0]) . '.php')){
+                    echo '<br> Changing current controller <br>';
+                    // Sets current controller if the first section of the URL is in the controllers folder
+                    $this->currentController = ucwords($currentUrl[0]);
+                    // Unsets the 0 index
+                    unset($currentUrl[0]);
+                }
+                // Require the controller
+                require_once '../app/controllers/' . $this->currentController . '.php';
+
+                // Instantiate the controller class
+                $this->currentController = new $this->currentController;
             }
 
             public function getUrl(){
@@ -21,6 +34,8 @@
                     $currentUrl = filter_var($currentUrl, FILTER_SANITIZE_URL);
                     $currentUrl = explode('/', $currentUrl);
                     return $currentUrl;
-                } 
+                } else {
+                    return [0=>'nothing'];
+                }
             }
     }
