cc4f9a1a7cbf7d44df4de0d55b5a3d992e2c9512
PsimanUK <simeonparris@hotmail.com>
2020-08-19 13:01:13 +0100

    add constant global variables to config.php for APPROOT, URLROOT and SITENAME.

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+    // App Root
+    define('APPROOT', dirname(dirname(__FILE__)) . '<br>'); 
+
+    //URL Root
+    define('URLROOT', 'http://localhost/PsimanMVC');
+
+    // Site Name
+    define('SITENAME', 'PsimanMVC');
